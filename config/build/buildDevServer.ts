7c965332676b61105e4e2cc1e286ff128aac6b05
gidgendugov <shinigami0202@mail.ru>
2025-07-12 15:53:22 +0300

    add Code splitting Lazy Suspence

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,6 +3,7 @@ import type { Configuration as DevServerConfigurator } from 'webpack-dev-server'
 export function buildDevServer(options: BuildOptions): DevServerConfigurator {
     return {
         port: options.port,
-        open: true
+        open: true,
+        historyApiFallback: true
     }
 }
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            MainPAge
+        </div>
+    );
+};
+
+export default MainPage;
