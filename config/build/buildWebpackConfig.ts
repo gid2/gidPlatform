37f743f37024b94173d1f74aeb1aef5ffd609bfe
gidgendugov <shinigami0202@mail.ru>
2025-07-14 20:21:31 +0300

    добавление архитектуры, AppRouter конфиг для роутера и Navbar

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,6 +1,5 @@
 import {BuildOptions} from "./types/config";
 import webpack from "webpack";
-import path from "path";
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
@@ -25,7 +24,7 @@ return {
     module: {
         rules: buildLoaders(options)
     },
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     devtool: isDev ? 'inline-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
 }
