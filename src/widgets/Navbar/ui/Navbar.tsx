1d36c60069f6d3495fdcae2d13750fb164c3c68c
gidgendugov <shinigami0202@mail.ru>
2025-08-20 19:51:31 +0300

    чиню eslint storybook делаю модальное окно и сайдбар

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -10,8 +10,7 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => (
     <div className={classNames(cls.navbar, {}, [className])}>
         <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to="/">Главная</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">О Сайте</AppLink>
+         /
         </div>
     </div>
 );
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,12 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import React, { useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
-import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
+import {classNames} from 'shared/lib/classNames/classNames';
+import React, {useState} from 'react';
+import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher';
+import {LangSwitcher} from 'shared/ui/LangSwitcher/LangSwitcher';
+import {Button, ButtonSize, ThemeButton} from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {RoutePath} from "shared/config/routeConfig/routeConfig";
+import {useTranslation} from "react-i18next";
 
 interface SidebarProps {
     className?: string
@@ -15,6 +18,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
+    const {t} = useTranslation()
 
     return (
         <div
@@ -24,9 +28,28 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 type="button"
                 onClick={onToggle}
                 className={cls.collapseBtn}
+                theme={ThemeButton.BACKGROUND_INVERTED}
+                square
+                size={ButtonSize.L}
             >
                 {collapsed ? '>' : '<'}
             </Button>
+            <div className={cls.items}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    className={cls.link}
+                    to={RoutePath.main}
+                >
+                    {t('Главная')}
+                </AppLink>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    className={cls.link}
+                    to={RoutePath.about}
+                >
+                    {t('О cайте')}
+                </AppLink>
+            </div>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
